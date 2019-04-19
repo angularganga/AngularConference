15ec32d29d8e8770cfc5e9818ba11b098cb08676
angularganga <ganganallur@gmail.com>
2019-04-19 15:19:40 -0400

    Routine update

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -7,8 +7,13 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 export class EventThumbnailComponent {
   @Input() angularEvent: any;
   @Output() eventClick: EventEmitter<string> = new EventEmitter<string>();
+  someProperty = "Some property";
 
   handleClickMe() {
     this.eventClick.emit(this.angularEvent.name);
+  }
+
+  logFoo() {
+    console.log("Log foo");
   }
 }
